8cce817a817c3149415c436cf87edfcdf59480fc
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-11 16:08:48 -0600

    step 6 and 7 solved

--- server/controller.js
+++ server/controller.js
@@ -29,7 +29,9 @@ module.exports = {
     approveAppointment: (req, res) => {
         let {apptId} = req.body
     
-        sequelize.query(`*****YOUR CODE HERE*****
+        sequelize.query(`update cc_appointments
+        set approved = true
+        where appt_id = ${apptId};
         
         insert into cc_emp_appts (emp_id, appt_id)
         values (${nextEmp}, ${apptId}),
@@ -71,6 +73,17 @@ module.exports = {
         join cc_users u on e.user_id = u.user_id
         where a.approved = true and a.completed = true
         order by a.date desc;`)
+        .then(dbRes => {
+            res.status(200).send(dbRes[0]);
+        })
+        .catch(err => console.log(err));
+    },
+    completeAppointment: (req, res) => {
+        let {apptId} = req.body
+
+        sequelize.query(`update cc_appointments
+        set completed = true
+        where appt_id = ${apptId}`)
         .then(dbRes => {
             res.status(200).send(dbRes[0]);
         })
