5e8c177464208b45edbd4712c6e15b5ee9e732c2
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-11 16:27:13 -0600

    subq prob2 solved

--- subqueries.sql
+++ subqueries.sql
@@ -3,3 +3,10 @@ select * from invoice
 where invoice_id in (
   select invoice_id from invoice_line where unit_price > 0.99
 );
+
+2.
+select *
+from playlist_track
+where playlist_id in (
+  select playlist_id from playlist where name = 'Music'
+ );
